09a6d04a98129a5076346d325a49676ee7652f53
AlexeyKosov <foster-13@yandex.ru>
2022-09-22 08:21:15 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+// import { resizeContent } from './functions/resize';
+// import { scrollTo } from './functions/smooth-scroll';
+// import { disableScroll, enableScroll } from './functions/stop-scroll';
+// import './vendor/focus-visible.min.js';
